29ea88f32174d28c46a8340426add48e34939af7
Thomas <thomas.fuchs6@etu.univ-lorraine.fr>
2024-11-07 09:00:18 +0100

    classe Spectacle

---
+++ Spectacle.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Class Spectacle
+ */
+class Spectacle
+{
+    /**
+     * @var string
+     */
+    private string $titre;
+    private string $description;
+    private string $urlVideo;
+    private string $horairePrevisionnel;
+
+    /**
+     * @var array
+     */
+    private array $images;
+    private array $artistes;
+
+
+    /**
+     * @param string $titre
+     * @param string $description
+     * @param string $urlVideo
+     * @param string $horairePrevisionnel
+     * @param array $images
+     * @param array $artistes
+     */
+    public function __construct(string $titre, string $description, string $urlVideo, string $horairePrevisionnel, array $images, array $artistes)
+    {
+        $this->titre = $titre;
+        $this->description = $description;
+        $this->urlVideo = $urlVideo;
+        $this->horairePrevisionnel = $horairePrevisionnel;
+        $this->images = $images;
+        $this->artistes = $artistes;
+    }
+
+
+    /**
+     * Magic get method
+     * @param string $name
+     * @return mixed
+     */
+    public function __get(string $name)
+    {
+        if (property_exists($this, $name)) {
+            return $this->$name;
+        }
+        throw new InvalidArgumentException("Property {$name} does not exist");
+    }
+
+    /**
+     * Magic set method
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __set(string $name, $value): void
+    {
+        if (property_exists($this, $name)) {
+            $this->$name = $value;
+        } else {
+            throw new InvalidArgumentException("Property {$name} does not exist");
+        }
+    }
+
+
+}
