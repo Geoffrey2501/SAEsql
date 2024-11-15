fa1caa653c49de66ca0b9666c0941494ac368fa8
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2024-11-15 16:52:26 +0100

    Update Dispatcher.php

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -113,7 +113,6 @@ class Dispatcher
         <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
         <title>NRV Festival</title>
         <style>
         /* Style général */
